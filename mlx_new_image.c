fe7922a8c6b91914b49a625172eb64cba7522abc
LONDARDI <LONDARDI@OSS>
2025-03-19 15:40:02 +0100

    mlx_new_image.c: silence unused-result warning in shm_att_pb using GCC diagnostic pragmas

--- mlx_new_image.c
+++ mlx_new_image.c
@@ -24,7 +24,12 @@ int	mlx_X_error;
 int	shm_att_pb(Display *d,XErrorEvent *ev)
 {
   if (ev->request_code==146 && ev->minor_code==X_ShmAttach)
-    write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
+  {
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wunused-result"
+    write(2, WARN_SHM_ATTACH, strlen(WARN_SHM_ATTACH));
+    #pragma GCC diagnostic pop
+  }
   mlx_X_error = 1;
 }
 
