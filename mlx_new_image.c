3076996a3546e90fe603ee0c81404c6cfff1982f
Liedson Habacuc <lhabacuc@c1r11s10.42luanda.com>
2025-07-25 00:50:26 +0100

    Fix warning: ignoring return value of write in shm_att_pb

--- mlx_new_image.c
+++ mlx_new_image.c
@@ -21,13 +21,16 @@
 
 int	mlx_X_error;
 
-int	shm_att_pb(Display *d,XErrorEvent *ev)
+int shm_att_pb(Display *d, XErrorEvent *ev)
 {
-  if (ev->request_code==146 && ev->minor_code==X_ShmAttach)
-    write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
-  mlx_X_error = 1;
+    if (ev->request_code == 146 && ev->minor_code == X_ShmAttach) {
+        ssize_t result = write(2, WARN_SHM_ATTACH, strlen(WARN_SHM_ATTACH));
+        if (result == -1)
+            perror("write failed");
+    }
+    mlx_X_error = 1;
+    return 0;
 }
-
 
 /*
 **  Data malloc :  width+32 ( bitmap_pad=32 ),    *4 = *32 / 8bit
